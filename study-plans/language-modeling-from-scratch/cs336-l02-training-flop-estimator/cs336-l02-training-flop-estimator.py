fd626b883d1c41f763b896fff633b21cdefdd54c
chirag <143872846+ctxnn@users.noreply.github.com>
2026-09-13 12:04:03 +0530

    Solve cs336-l02-training-flop-estimator

---
+++ study-plans/language-modeling-from-scratch/cs336-l02-training-flop-estimator/cs336-l02-training-flop-estimator.py
@@ -0,0 +1,16 @@
+def flop_estimator(matmuls, attention_flops=0):
+    """
+    Returns: dictionary containing exact forward, backward, and total FLOP counts
+    """
+    forward_flops = 0
+    for i in matmuls:
+        forward_flops += 2 * i[0] * i[1] * i[2]
+    forward_flops += attention_flops
+    backward_flops = 2 * forward_flops 
+    total_flops = forward_flops + backward_flops 
+    return {
+        'forward_flops':forward_flops,
+        'backward_flops':backward_flops,
+        'total_flops':total_flops
+    }
+    
